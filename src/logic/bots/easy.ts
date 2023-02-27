cac77e3d15766b3a3af5ec7e6e27678d6f651ba7
Sarah Hägele <sarah.haegele01@gmail.com>
2023-02-27 11:06:53 +0100

    feat(easyBot): implemented easyMove function
    feat(easyBot): used randomMove function from bot.ts
    
    feat(easyBot): used winningMove function from bot.ts
    
    fix(easyBot): checked for full board state

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,8 +1,24 @@
-import type { Field } from "../game";
+import { Field, getBlanks } from "../game";
+import { randomMove, winningMove } from "./bot";
 
 // the easy bot:
 // - chooses the winning move, if it can win
 // - chooses a random move otherwise
 export function easyMove(board: Field[], own: Field): number {
-  return -1 // TODO: implement
+  // check for a winning move
+  var fieldNo = winningMove(board, own);
+
+  if (fieldNo == -1 && getBlanks(board).length > 0) {
+    // no winning move possible - choose a random move
+    var empty = false;
+    while (!empty) {
+      // choose random field number
+      fieldNo = randomMove(9);
+      // check if field is empty
+      if (board[fieldNo] == Field.EMPTY) {
+        empty = true;
+      }
+    }
+  }
+  return fieldNo;
 }
