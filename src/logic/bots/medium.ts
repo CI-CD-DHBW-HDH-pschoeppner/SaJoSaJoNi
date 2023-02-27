c17a775cf45e70407676a1d7e114e9d7b6ad322e
JohannesAuberger <77609143+JohannesAuberger@users.noreply.github.com>
2023-02-27 11:48:38 +0100

    bugfix2

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -11,66 +11,35 @@ export function mediumMove(board: Field[], own: Field): number {
   var goalField = -1;
 
   // chooses the winning move, if it can win
-  for(const move of blanks) {
-    board[move] = own;
-    if(won(board) === own) {
+  for (const move of blanks) {
+    const copyBoard = [...board];
+    copyBoard[move] = own;
+    if (won(copyBoard) === own) {
       goalField = move;
       break;
     }
-    board[move] = Field.EMPTY;
   }
 
   if (goalField === -1) {
     // blocks the player from winning, if it can
-    // horizontal
-    for (let i = 0; i < 9; i = i + 3) {
-      if (board[i] === 1 && board[i + 1] === 1 && board[i + 2] === 0) {
-        goalField = i + 2;
+    for (const move of blanks) {
+      const copyBoard = [...board];
+      copyBoard[move] = invertPlayer(own);
+      if (won(copyBoard) === invertPlayer(own)) {
+        goalField = move;
+        break;
       }
-      if (board[i] === 0 && board[i + 1] === 1 && board[i + 2] === 1) {
-        goalField = i;
-      }
-      if(board[i] === 1 && board[i + 1] === 0 && board[i + 2] === 1) {
-        goalField = i + 1;
-      }
-    }
-
-    // vertical
-    for (let i = 0; i < 3; i++) {
-      if (board[i] === 1 && board[i + 3] === 1 && board[i + 6] === 0) {
-        goalField = i + 6;
-      }
-      if (board[i] === 0 && board[i + 3] === 1 && board[i + 6] === 1) {
-        goalField = i;
-      }
-      if(board[i] === 1 && board[i + 3] === 0 && board[i + 6] === 1) {
-        goalField = i + 3;
-      }
-    }
-
-    // diagonal
-    if (board[0] === 1 && board[4] === 1 && board[8] === 0) {
-      goalField = 8;
-    }
-    if (board[0] === 0 && board[4] === 1 && board[8] === 1) {
-      goalField = 0;
-    }
-    if (board[2] === 1 && board[4] === 1 && board[6] === 0) {
-      goalField = 6;
-    }
-    if (board[2] === 0 && board[4] === 1 && board[6] === 1) {
-      goalField = 2;
     }
   }
 
-  if(goalField === -1) {
+  if (goalField === -1) {
     // chooses the middle (4) field, if it can
-    if(board[4] === Field.EMPTY) {
+    if (board[4] === Field.EMPTY) {
       goalField = 4;
     }
   }
 
-  if(goalField === -1) {
+  if (goalField === -1) {
     // chooses a random move otherwise
     goalField = blanks[Math.floor(Math.random() * blanks.length)];
   }
